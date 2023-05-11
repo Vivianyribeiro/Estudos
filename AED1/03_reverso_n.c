df478e19fc08ee8983b9432f107f1244281dd3c9
Viviany Ribeiro Batista <109182145+Vivianyribeiro@users.noreply.github.com>
2023-05-11 18:24:45 -0300

    Create 03_reverso_n

---
+++ AED1/03_reverso_n.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int digitos(int n)
+{
+    int i;
+    for (i = 1; n >= 10; i++)
+    {
+        n /= 10;
+    }
+    return i;
+}
+
+int reverso(int n, int n_reverso){
+    int aux = 0;
+    if (n == 0 ){
+        return n_reverso;
+    } else {
+        int digito = n % 10;
+        n_reverso = (n_reverso * 10) + digito;
+        return reverso(n / 10, n_reverso);
+    }
+    
+}   
+
+int main(){
+    int n;
+    int n_reverso = 0;
+    scanf ("%d", &n);
+    
+    n_reverso = reverso (n, n_reverso);
+    printf("%d", n_reverso);
+
+    return 0;
+}
