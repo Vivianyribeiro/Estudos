9a466bb4cfd7d31678274a4c1b732c6951d677c1
Viviany Ribeiro Batista <109182145+Vivianyribeiro@users.noreply.github.com>
2023-05-17 19:49:42 -0300

    Update 05_banco_inteligente.c

--- AED1/lista_de_exercicios2/05_banco_inteligente.c
+++ AED1/lista_de_exercicios2/05_banco_inteligente.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#define max 6
+
+typedef struct Notas {
+    int valor;
+    int qtde;
+}nota;
+
+void recursao (int s, int notas[]){
+    if(s % n[6].valor > 0) {
+        n[6].qtde = s / n[6].valor;
+        
+    }
+
+}
 
 int main() {
     int saque;
-    int n2, n5, n10, n20, n50, n100;
+    int notas[max];
+    //int n2, n5, n10, n20, n50, n100;
 
     scanf("%d", &saque);
-    scanf("%d %d %d %d %d %d ", &n2, &n5, &n10, &n20, &n50, &n100);
-    
-
-
-    printf("");
+    for(int i = 0; i < 6; i++){
+        scanf ("%d", &notas[i]);
+    }
 
 
     return 0;
